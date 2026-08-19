7af8af37af48be79be575d166a31f478f4e1b725
Amanda Ferreira <ferreiradejesusamanda@gmail.com>
2026-08-19 12:39:15 +0000

    Exercícios utilizando estruturas de seleção

---
+++ classroom/02-flow/submissions/amanda-ferreira/src/Positivonegativo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+    public class Positivonegativo {
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            int numero = scanner.nextInt();
+            String valor;
+
+            if(numero > 0)
+                valor = "POSITIVO";
+            else if(numero < 0)
+                valor = "NEGATIVO";
+            else
+                valor = "ZERO";
+
+            System.out.println(""+ valor +"");
+        }
+    } 
